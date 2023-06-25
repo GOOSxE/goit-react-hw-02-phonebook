4f5dcd2b8b0e41216ef41402f41b344dd17d9a4f
GOOSxE <der.goose.4308@gmail.com>
2023-06-25 16:27:35 +0200

    Update Contact-form.jsx

--- src/components/Contact-form.jsx
+++ src/components/Contact-form.jsx
@@ -33,6 +33,7 @@ export default class ContactForm extends React.Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            value={this.state.name}
           ></input>
         </label>
         <label>
@@ -44,6 +45,7 @@ export default class ContactForm extends React.Component {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
+            value={this.state.number}
           />
         </label>
 
@@ -52,5 +54,3 @@ export default class ContactForm extends React.Component {
     );
   }
 }
-
-
